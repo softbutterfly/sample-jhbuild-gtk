350c4d0ce8ad813c729cfbf8ea88c819cc6fbd7d
zodiacfireworks <martin.vuelta@gmail.com>
2017-02-11 17:47:33 -0500

    :lollipop: styles sample code

--- samples/sample3.py
+++ samples/sample3.py
@@ -1,14 +1,35 @@
 import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
+from gi.repository import Gdk
+
+css = b"""
+#hello_world_window {
+    background-color: red;
+}
+
+#hello_world_button {
+    background-color: blue;
+}
+"""
+
+style_provider = Gtk.CssProvider()
+style_provider.load_from_data(css)
+
 
 class HelloWorldWindow(Gtk.Window):
 
     def __init__(self):
         Gtk.Window.__init__(self, title="Hello World")
 
+        self.set_name('hello_world_window')
+        self.set_border_width(10)
+
+
         self.button = Gtk.Button(label="Click Here")
+        self.button.set_name('hello_world_button')
         self.button.connect("clicked", self.on_button_clicked)
+
         self.add(self.button)
 
     def on_button_clicked(self, widget):
@@ -17,6 +38,13 @@ class HelloWorldWindow(Gtk.Window):
 
 def main():
     win = HelloWorldWindow()
+
+    Gtk.StyleContext.add_provider_for_screen(
+        Gdk.Screen.get_default(),
+        style_provider,
+        Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION
+    )
+
     win.connect("delete-event", Gtk.main_quit)
     win.show_all()
     Gtk.main()
